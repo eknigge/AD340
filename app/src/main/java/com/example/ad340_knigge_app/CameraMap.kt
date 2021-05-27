54d8df7a883606f32b825048f07eebd8692183df
Eric Knigge <ericknigge@gmail.com>
2021-05-26 20:05:41 -0700

    Updated activity permissions

--- app/src/main/java/com/example/ad340_knigge_app/CameraMap.kt
+++ app/src/main/java/com/example/ad340_knigge_app/CameraMap.kt
@@ -3,76 +3,112 @@ package com.example.ad340_knigge_app
 import android.Manifest
 import android.content.pm.PackageManager
 import android.location.Location
-import android.net.ConnectivityManager
-import android.net.NetworkCapabilities
 
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
-import com.example.ad340_knigge_app.Model.Camera
+import androidx.core.content.ContextCompat
 import com.example.ad340_knigge_app.Model.CameraLocation
 import com.example.ad340_knigge_app.Model.CameraModel
+import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.*
+import com.google.android.gms.maps.model.BitmapDescriptor
 
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.gson.JsonArray
-import com.google.gson.JsonObject
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import retrofit2.Call
-import retrofit2.await
 import retrofit2.awaitResponse
-import javax.security.auth.callback.Callback
-import kotlin.reflect.typeOf
 
 internal class CameraMap: AppCompatActivity(), OnMapReadyCallback {
+    private val PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 100
     private lateinit var mMap: GoogleMap
+    private var locationPermissionGranted = false
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private val SEATTLE = LatLng(47.6081206,-122.3370977)
+    private val DEFAULT_ZOOM = 12f
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.fragment_camera_map)
+
+        // Set action bar title
+        supportActionBar?.title = "Traffic Cam Map"
+
+        // check permissions
+        getLocationPermission()
+
+        // Construct a FusedLocationProviderClient
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
                 .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
     }
 
-    fun getLastKnownLocation(){
-        val fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
-        Log.d("DATA", "Step 1")
-        if (ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            Log.d("DATA", "Step 2")
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
-                Log.d("DATA", "Step 3")
-                if(location != null){
-                    Log.d("DATA", "Step 4")
-                    Log.d("DATA", location.toString())
-                    centerMapOnUser(location)
+    fun getLocationPermission(){
+        if(ContextCompat.checkSelfPermission(this.applicationContext,
+                Manifest.permission.ACCESS_FINE_LOCATION) ==
+                PackageManager.PERMISSION_GRANTED){
+            locationPermissionGranted = true
+        } else {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION)
+        }
+    }
+
+    fun getDeviceLocation(){
+        try {
+            if (locationPermissionGranted) {
+                val locationResult = fusedLocationProviderClient.lastLocation
+                locationResult.addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Set the map's camera position to the current location of the device.
+                        val lastKnownLocation = task.result
+                        if (lastKnownLocation != null) {
+                            // move camera
+                            mMap?.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                                LatLng(lastKnownLocation!!.latitude,
+                                    lastKnownLocation!!.longitude), 14f))
+                            // add marker
+                            mMap?.addMarker(
+                                MarkerOptions()
+                                    .position(
+                                        LatLng(lastKnownLocation.latitude,
+                                        lastKnownLocation.longitude))
+                                    .title("Current Location")
+                                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE))
+                            )
+                        }
+                    } else {
+                        Log.d("ERROR", "Current location is null. Using defaults.")
+                        Log.e("ERROR", "Exception: %s", task.exception)
+                        mMap?.moveCamera(CameraUpdateFactory
+                            .newLatLngZoom(SEATTLE, DEFAULT_ZOOM))
+                        mMap?.uiSettings?.isMyLocationButtonEnabled = false
+                    }
                 }
             }
-            return
+        } catch (e: SecurityException) {
+            Log.e("Exception: %s", e.message, e)
         }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 
     fun centerMapOnUser(location: Location){
@@ -93,18 +129,14 @@ internal class CameraMap: AppCompatActivity(), OnMapReadyCallback {
         mMap = googleMap
         mMap.moveCamera(CameraUpdateFactory.zoomTo(12f))
 
-        // Add a marker in Sydney and move the camera
-        val seattle = LatLng(47.6081206,-122.3370977)
-        mMap.addMarker(MarkerOptions()
-                .position(seattle)
-                .title("Marker on Seattle"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(seattle))
+        // Center map in Seattle, re-center when user location available
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(SEATTLE))
 
-        getLocationData()
+        getCameraData()
 
     }
 
-    fun getLocationData(){
+    fun getCameraData(){
         val api = ApiObject.getApiObject()!!
         CoroutineScope(Dispatchers.IO).launch{
             val response = api.getTrafficCamData().awaitResponse()
@@ -113,7 +145,7 @@ internal class CameraMap: AppCompatActivity(), OnMapReadyCallback {
                 val locationData = apiToList(data)
 
                 withContext(Dispatchers.Main){
-                    getLastKnownLocation()
+                    getDeviceLocation()
                     updateMap(locationData)
                 }
             }
