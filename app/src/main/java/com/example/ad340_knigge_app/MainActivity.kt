0737a4dca3835a21cd4a9cbab4f18dfd3dc6cfdc
Eric Knigge <ericknigge@gmail.com>
2021-06-09 16:41:02 -0700

    Added validation methods

--- app/src/main/java/com/example/ad340_knigge_app/MainActivity.kt
+++ app/src/main/java/com/example/ad340_knigge_app/MainActivity.kt
@@ -1,16 +1,21 @@
 package com.example.ad340_knigge_app
 
+import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
-import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.UserProfileChangeRequest
 
 class MainActivity : AppCompatActivity() {
     var mCount = 0
+    lateinit var inputUsername: String
+    lateinit var inputEmail: String
+    lateinit var inputPassword: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,14 +24,14 @@ class MainActivity : AppCompatActivity() {
         // Link to movie activity
         var movieButton = findViewById<Button>(R.id.movies_button)
         movieButton.setOnClickListener {
-            val intent = Intent(this , Movies::class.java)
+            val intent = Intent(this, Movies::class.java)
             startActivity(intent)
         }
 
         // Link to traffic activity
         var trafficButton = findViewById<Button>(R.id.traffic_camera_button)
         trafficButton.setOnClickListener {
-            val intent = Intent(this , TrafficCameras::class.java)
+            val intent = Intent(this, TrafficCameras::class.java)
             startActivity(intent)
         }
 
@@ -34,14 +39,44 @@ class MainActivity : AppCompatActivity() {
         var cameraMapButton = findViewById<Button>(R.id.camera_map_button)
         cameraMapButton.setOnClickListener{
             Log.d("LOG", "Progress item")
-            val intent = Intent(this , CameraMap::class.java)
+            val intent = Intent(this, CameraMap::class.java)
             startActivity(intent)
+        }
+
+        var loginButton = findViewById<Button>(R.id.login_button)
+        loginButton.setOnClickListener{
+
+            // initialize input variables
+            inputEmail = findViewById(R.id.email_input)
+            inputPassword = findViewById(R.id.password_input)
+            inputUsername = findViewById(R.id.username_input)
+
+            // validate inputs
+            if(areInputsValid()){
+                // if valid run login
+                signIn()
+            }
         }
 
     }
 
+    fun isEmailValid(email: String): Boolean{
+        if(email.isEmpty()){
+            return false
+        } else {
+            return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()
+        }
+    }
+
+    fun areInputsValid(): boolean {
+        val email = isEmailValid(inputEmail.toString())
+        val username = inputUsername.toString().isEmpty()
+        val password = inputPassword.toString().isEmpty()
+        return email && username && password
+    }
+
     fun sendToast(view: View) {
-        Toast.makeText(this,"Message Sent",Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, "Message Sent", Toast.LENGTH_SHORT).show()
     }
 
     fun runNetflix(view: View) {
@@ -58,6 +93,72 @@ class MainActivity : AppCompatActivity() {
 
     fun runLiveTV(view: View) {
         Toast.makeText(this, "Watch Live TV", Toast.LENGTH_LONG).show()
+    }
+
+    fun saveToSharedPreferences(email: String, username:String, password:String){
+        val sharedPref = this.getPreferences(Context.MODE_PRIVATE) ?: return
+        with (sharedPref.edit()){
+            putString("email", email)
+            putString("username", username)
+            putString("password", password)
+            apply()
+        }
+    }
+
+    fun getSharedPreferenceValues(): List<String>{
+        val sharedPref = this.getPreferences(Context.MODE_PRIVATE)
+        val sharedPrefData = ArrayList<String>()
+        sharedPrefData.add(sharedPref.getString("email", "").toString())
+        sharedPrefData.add(sharedPref.getString("username", "").toString())
+        sharedPrefData.add(sharedPref.getString("password", "").toString())
+        return sharedPrefData
+    }
+
+    fun signIn(){
+        Log.d("FIREBASE", "signIn")
+
+        // 2 - save valid entries to shared preferences
+
+
+        // 3 - sign into Firebase
+        val mAuth = FirebaseAuth.getInstance();
+        mAuth.signInWithEmailAndPassword(inputEmail.toString(), inputPassword.toString())
+                .addOnCompleteListener(this,
+
+
+                        new OnCompleteListener < AuthResult >() {
+
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d("FIREBASE", "signIn:onComplete:" + task.isSuccessful())
+
+                        if (task.isSuccessful()) {
+                            // update profile. displayname is the value entered in UI
+                            FirebaseUser user = FirebaseAuth . getInstance ().getCurrentUser()
+                            UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                                    .setDisplayName(displayname)
+                                    .build()
+
+                            user.updateProfile(profileUpdates)
+                                    .addOnCompleteListener(new OnCompleteListener < Void >() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if (task.isSuccessful()) {
+                                                Log.d("FIREBASE", "User profile updated.")
+                                                // Go to FirebaseActivity
+                                                startActivity(new Intent MainActivity.this, FirebaseActivity.class))
+                                            }
+                                        }
+                                    })
+
+                        } else {
+                            Log.d("FIREBASE", "sign-in failed")
+
+                            Toast.makeText(MainActivity.this, "Sign In Failed",
+                                    Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                })
     }
 
 
